4fe1f4ec89b2bb19c38c587f9f231783ac10a6ab
SawyerOG <oglesbysaw@gmail.com>
2025-08-23 13:00:36 -0600

    handle laptop monitor correctly

--- hypr/.config/hypr/monitors.conf.sh
+++ hypr/.config/hypr/monitors.conf.sh
@@ -3,8 +3,8 @@
 # Detect if laptop internal screen (eDP) exists
 if ls /sys/class/drm | grep -q "^card[0-9]-eDP"; then
     # Laptop mode
-    echo "monitor=eDP-1,preferred,0x0,auto"
-    echo "monitor=HDMI-A-1,preferred,1920x0,auto"
+    echo "monitor=eDP-1,preferred,0x0,1"
+    #echo "monitor=HDMI-A-1,preferred,1920x0,auto"
 else
     # Desktop mode
     echo "monitor=HDMI-A-1,preferred,0x0,auto"
