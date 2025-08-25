584e0a0c68c1f6d355405f176264785708b72a57
SawyerOG <oglesbysaw@gmail.com>
2025-08-25 16:35:35 -0600

    a few more apps

--- install_arch.sh
+++ install_arch.sh
@@ -20,7 +20,8 @@ echo "==> Installing packages..."
 sudo pacman -S --needed --noconfirm \
   git ghostty stow hyprland hyprlock hypridle swaync neovim openssh \
   ttf-jetbrains-mono wireplumber wiremix lazygit lazydocker go fastfetch wl-clipboard \
-  waybar emptty unzip fd
+  waybar emptty unzip fd ripgrep networkmanager networkmanager-openconnect openconnect \
+  network-manager-applet libreoffice-fresh noto-fonts noto-fonts-emoji ttf-dejavu ttf-liberation
 
 echo "==> Setting up Chaotic-AUR..."
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
