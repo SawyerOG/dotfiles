72848d38863066cd979c3e07558e448f03414113
SawyerOG <oglesbysaw@gmail.com>
2025-09-06 13:37:25 -0600

    conform

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,17 +1,17 @@
 return {
   {
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     -- dependencies = { 'nvim-tree/nvim-web-devicons' }
     lazy = true,
     event = "BufReadPre",
 
     config = function()
-      require('lualine').setup {
+      require("lualine").setup {
         options = {
           icons_enabled = true,
-          theme = 'auto',
-          component_separators = { left = '', right = '' },
-          section_separators = { left = '', right = '' },
+          theme = "auto",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
           disabled_filetypes = {
             statusline = {},
             winbar = {},
@@ -24,29 +24,29 @@ return {
             statusline = 100,
             tabline = 100,
             winbar = 100,
-          }
+          },
         },
         sections = {
-          lualine_a = { 'mode' },
-          lualine_b = { 'branch' },
-          lualine_c = { 'diagnostics', 'filename' },
-          lualine_x = { 'filesize', 'fileformat', 'filetype' },
+          lualine_a = { "mode" },
+          lualine_b = { "branch" },
+          lualine_c = { "diagnostics", "filename" },
+          lualine_x = { "filesize", "fileformat", "filetype" },
           lualine_y = {},
-          lualine_z = { 'location' }
+          lualine_z = { "location" },
         },
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = { 'filename' },
+          lualine_c = { "filename" },
           lualine_x = {},
           lualine_y = {},
-          lualine_z = {}
+          lualine_z = {},
         },
         tabline = {},
         winbar = {},
         inactive_winbar = {},
-        extensions = {}
+        extensions = {},
       }
-    end
-  }
+    end,
+  },
 }
