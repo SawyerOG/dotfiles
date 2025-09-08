3b2bce6b1a71cf9f4ecee356f63f40a8cf0477eb
SawyerOG <oglesbysaw@gmail.com>
2025-09-08 10:29:26 -0600

    clean up

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,4 @@
 return {
-  -- "neovim/nvim-lspconfig",
-  -- dependencies = {
-  --   { "mason-org/mason.nvim", opts = {} },
-  --   "mason-org/mason-lspconfig.nvim",
-  --   -- "WhoIsSethDaniel/mason-tool-installer.nvim",
-  --   { "j-hui/fidget.nvim", opts = {} },
-  -- },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
