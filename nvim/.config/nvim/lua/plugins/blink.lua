e05794d7beed3d57305d57357903e646e27df660
SawyerOG <oglesbysaw@gmail.com>
2025-09-08 10:29:22 -0600

    dadbod

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,32 +1,40 @@
 return {
   {
     "saghen/blink.cmp",
-    dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "kristijanhusak/vim-dadbod-completion",
+    },
     event = "BufReadPre",
     lazy = true,
-
-    -- use a release tag to download pre-built binaries
     version = "1.*",
-    opts = {
-      keymap = { preset = "default" },
-      appearance = {
-        nerd_font_variant = "mono",
-      },
-      completion = { documentation = { auto_show = true } },
-      fuzzy = { implementation = "prefer_rust_with_warning" },
-      signature = { enabled = true },
-      -- sources = {
-      --   default = { 'avante', 'lsp', 'path', 'snippets', 'buffer' },
-      --   providers = {
-      --     avante = {
-      --       module = 'blink-cmp-avante',
-      --       name = 'Avante',
-      --       opts = {
-      --         -- options for blink-cmp-avante
-      --       }
-      --     },
-      --   },
-      -- },
-    },
+    opts = function(_, opts)
+      -- preserve existing Blink options
+      opts.keymap = opts.keymap or { preset = "default" }
+      opts.appearance = opts.appearance or { nerd_font_variant = "mono" }
+      opts.completion = opts.completion or { documentation = { auto_show = true } }
+      opts.fuzzy = opts.fuzzy or { implementation = "prefer_rust_with_warning" }
+      opts.signature = opts.signature or { enabled = true }
+
+      -- ensure sources table exists
+      opts.sources = opts.sources or {}
+      opts.sources.default = opts.sources.default or { "lsp", "path", "snippets", "buffer" }
+
+      -- add Dadbod to default sources if not already present
+      if not vim.tbl_contains(opts.sources.default, "dadbod") then
+        table.insert(opts.sources.default, "dadbod")
+      end
+
+      -- register Dadbod as a provider
+      opts.sources.providers = vim.tbl_deep_extend("force", opts.sources.providers or {}, {
+        dadbod = {
+          module = "vim_dadbod_completion.blink",
+          name = "Dadbod",
+          min_keyword_length = 0,
+        },
+      })
+
+      return opts
+    end,
   },
 }
